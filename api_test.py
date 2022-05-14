e1c54500f0d9a55f6dce7e6d9fbd20371e84b862
Bekzhan <bidahmetov2002@gmail.com>
2022-05-14 23:30:58 +0200

    Redesigned tests

--- api_test.py
+++ api_test.py
@@ -1,14 +1,9 @@
-from sys import api_version
 import unittest
 import requests
 
 class ApiTest(unittest.TestCase):
     BASE_URL = "http://127.0.0.1:5000/"
-    Product = "Laptop"
-    InitialAmount = 15
     BuyAmount = 5
-    FinalAmount = InitialAmount - BuyAmount
-    BuyPrice = 5200
 
     NewTestProduct = {
         'name': 'TestProduct',
@@ -22,33 +17,37 @@ class ApiTest(unittest.TestCase):
         'price': 150
     }
 
-    # def test_1_get_all_products(self):
-    #     r = requests.get(ApiTest.BASE_URL + "AllProducts")
-    #     self.assertEqual(r.status_code, 200)
-    #     #print(r.json())
-    #     self.assertEqual(len(r.json()), 1)
+    BuyPrice = (NewTestProduct['price'] * 5) + (NewTestProduct['price'] * 5) * 0.30
 
-    # def test_2_get_check_status(self):
-    #     r = requests.get(f"{ApiTest.BASE_URL}Status/{ApiTest.Product}")
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertEqual(r.json()['Product'], ApiTest.Product)
-    #     self.assertEqual(r.json()['Amount Available'], ApiTest.InitialAmount)
-
-    # def test_3_get_buy_product(self):
-    #     r = requests.get(f'{ApiTest.BASE_URL}Buy/{ApiTest.Product}/{ApiTest.BuyAmount}')
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertEqual(r.json()['cost'], ApiTest.BuyPrice)
-    #     # Checking status after Buy request
-    #     r = requests.get(f"{ApiTest.BASE_URL}Status/{ApiTest.Product}")
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertEqual(r.json()['Amount Available'], ApiTest.FinalAmount)
-
-    def test_4_post_create_product(self):
+    def test_1_post_create_product(self):
         r = requests.post(f'{ApiTest.BASE_URL}CreateProduct', json=ApiTest.NewTestProduct)
         self.assertEqual(r.status_code, 201)
         self.assertEqual(r.json()['name'], ApiTest.NewTestProduct['name'])
         self.assertEqual(r.json()['amount'], ApiTest.NewTestProduct['amount'])
         self.assertEqual(r.json()['price'], ApiTest.NewTestProduct['price'])
+
+    def test_2_get_all_products(self):
+        r = requests.get(ApiTest.BASE_URL + "AllProducts")
+        self.assertEqual(r.status_code, 200)
+        for p in r.json():
+            if p['name'] == ApiTest.NewTestProduct['name']:
+                self.assertEqual(p['amount'], ApiTest.NewTestProduct['amount'])
+                self.assertEqual(p['price'], ApiTest.NewTestProduct['price'])
+
+    def test_3_get_check_status(self):
+        r = requests.get(f"{ApiTest.BASE_URL}Status/{ApiTest.NewTestProduct['name']}")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json()['Product'], ApiTest.NewTestProduct['name'])
+        self.assertEqual(r.json()['Amount Available'], ApiTest.NewTestProduct['amount'])
+
+    def test_4_get_buy_product(self):
+        r = requests.get(f'{ApiTest.BASE_URL}Buy/{ApiTest.NewTestProduct["name"]}/{ApiTest.BuyAmount}')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json()['cost'], ApiTest.BuyPrice)
+        # Checking status after Buy request
+        r = requests.get(f"{ApiTest.BASE_URL}Status/{ApiTest.NewTestProduct['name']}")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json()['Amount Available'], ApiTest.NewTestProduct['amount'] - ApiTest.BuyAmount)
 
     def test_5_post_update_product(self):
         r = requests.post(f'{ApiTest.BASE_URL}ModifyProduct/{ApiTest.NewTestProduct["name"]}', json=ApiTest.NewTestProductUpdated)
